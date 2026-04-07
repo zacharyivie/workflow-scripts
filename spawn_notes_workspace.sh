c40bf6c144b5f5862802e40670c207ddc44c513e
Zachary Ivie <zachary.ivie@gmail.com>
2026-04-07 12:09:07 -0400

    - Set spawn_notes_workspace.sh to use static SecondBrain path for notes instead of taking PWD.

--- spawn_notes_workspace.sh
+++ spawn_notes_workspace.sh
@@ -2,4 +2,4 @@
 niri msg action focus-workspace 255
 obsidian & disown
 sleep 1
-/srv/dev/Projects/Scripts/spawn_claude_terminal.sh "$PWD"
+/srv/dev/Projects/Scripts/spawn_claude_terminal.sh '/srv/dev/Projects/SecondBrain'
